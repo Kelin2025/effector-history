1c6eafe63b898f91674db9c424ebb676bbee7ffc
Anton Kosykh <kelin2025@yandex.ru>
2023-03-09 18:15:25 +0300

    feat: New features

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,71 @@
-import {
-  Store,
-  createEvent,
-  createStore,
-  combine,
-  sample,
-  guard,
-  Clock,
-} from 'effector';
+import { createEvent, createStore, combine, sample, Store, Unit } from "effector";
 
-export const createHistory = <T extends Record<string, Store<any>>>(params: {
-  source: T;
-  clock?: Clock<unknown>;
-  maxLength: number;
+type HistoryStrategy = {};
+
+const initialTrigger = createEvent();
+const unknownTrigger = createEvent();
+const manualTrigger = createEvent();
+
+export const createHistory = <T extends Record<string, any> | unknown[]>(params: {
+  source: T extends unknown[] ? Store<T[keyof T]>[] : { [k in keyof T]: Store<T[k]> };
+  clock?: Unit<any>[];
+  strategies?: Map<Unit<any>, HistoryStrategy>;
+  maxLength?: number;
+  serialize?: "ignore";
 }) => {
-  const undo = createEvent();
-  const redo = createEvent();
-  const push = createEvent<T>();
-  const clear = createEvent();
+  const $source = combine(params.source) as Store<T>;
+  const strategies = params.strategies || new Map();
+  const maxLength = params.maxLength || 20;
 
-  const $history = createStore<T[]>([]);
-  const $curIndex = createStore(-1);
+  const undo = createEvent<any>();
+  const redo = createEvent<any>();
+  const push = createEvent<{ record: T; trigger: Unit<any> }>();
+  const replace = createEvent<{ record: T; trigger: Unit<any> }>();
+  const clear = createEvent<any>();
 
-  const $length = $history.map(history => history.length);
-  const $canUndo = $curIndex.map(idx => idx > 0);
-  const $canRedo = combine(
-    $curIndex,
-    $length,
-    (idx, historyLength) => idx + 1 < historyLength
+  // @ts-expect-error
+  const initialState: typeof params.source = Array.isArray(params.source) ? [] : {};
+  for (const idx in params.source) {
+    // @ts-expect-error
+    initialState[idx] = params.source[idx].defaultState;
+  }
+
+  const $history = createStore<typeof params.source[]>(
+    [initialState],
+    params.serialize ? { serialize: params.serialize } : {}
   );
-  const $curRecord = combine(
-    $history,
+  const $triggers = createStore<Unit<any>[]>([initialTrigger], {
+    serialize: "ignore",
+  });
+  const $curIndex = createStore(0);
+  const $curRecord = combine($history, $curIndex, (history, curIndex) => history[curIndex] || null);
+  const $curTrigger = combine(
+    $triggers,
     $curIndex,
-    (history, curIndex) => history[curIndex] || null
+    (triggers, curIndex) => triggers[curIndex] || unknownTrigger
   );
-  const $actualState = combine(params.source);
+  const $length = $history.map((history) => history.length);
+  const $canUndo = $curIndex.map((idx) => idx > 0);
+  const $canRedo = combine($curIndex, $length, (idx, historyLength) => idx + 1 < historyLength);
+  const $actualState = combine($source, $curRecord, (source, record) => record || source);
 
   // Push logic
   const pushed = sample({
-    source: [$curIndex, $history],
+    source: [$curIndex, $history] as const,
     clock: push,
-    fn: ([curIndex, history], record) => {
+    fn: ([curIndex, history], { record, trigger }) => {
       return {
         curIndex,
         record,
-        shouldPop: history.length >= params.maxLength,
+        trigger,
+        shouldPop: history.length >= maxLength,
       };
     },
   });
 
   $history.on(pushed, (prev, { curIndex, record, shouldPop }) => {
     const next = prev.slice(0, curIndex + 1);
+    // @ts-expect-error
     next.push(record);
     if (shouldPop) {
       next.pop();
@@ -57,46 +73,83 @@ export const createHistory = <T extends Record<string, Store<any>>>(params: {
     return next;
   });
 
-  $curIndex.on(pushed, (curIndex, { shouldPop }) =>
-    shouldPop ? curIndex : curIndex + 1
-  );
+  $triggers.on(pushed, (prev, { curIndex, trigger, shouldPop }) => {
+    const next = prev.slice(0, curIndex + 1);
+    next.push(trigger);
+    if (shouldPop) {
+      next.pop();
+    }
+    return next;
+  });
+
+  $curIndex.on(pushed, (curIndex, { shouldPop }) => (shouldPop ? curIndex : curIndex + 1));
+
+  // Replace logic
+  $history.on(replace, (prev, { record }) => {
+    const next = [...prev];
+    // @ts-expect-error
+    next[next.length - 1] = record;
+    return next;
+  });
 
   // Undo logic
-  const undoPassed = guard({
+  const undoPassed = sample({
     source: $curIndex,
     clock: undo,
     filter: $canUndo,
   });
 
-  $curIndex.on(undoPassed, idx => Math.max(0, idx - 1));
+  $curIndex.on(undoPassed, (idx) => Math.max(0, idx - 1));
 
   // Redo logic
-  const redoPassed = guard({
+  const redoPassed = sample({
     source: [$curIndex, $length],
     clock: redo,
     filter: $canRedo,
   });
 
-  $curIndex.on(redoPassed, idx => idx + 1);
+  $curIndex.on(redoPassed, (idx) => idx + 1);
 
   // Clear logic
   $curIndex.on(clear, () => 0);
-
-  // @ts-expect-error
-  $history.on(sample($actualState, clear), (history, actualState) => [
-    actualState,
-  ]);
+  $history.on(clear, () => [initialState]);
+  $triggers.on(clear, () => [initialTrigger]);
 
   const $shouldPush = createStore(true);
 
   // History sync logic
-  guard({
-    source: $actualState,
-    ...(params.clock ? { clock: params.clock } : {}),
-    filter: $shouldPush,
-    // @ts-expect-error
-    target: push,
-  });
+  const triggers = params.clock || params.source;
+
+  for (const trigger of Object.values(triggers)) {
+    const strategy = strategies.get(trigger) ?? pushStrategy;
+    switch (strategy) {
+      case replaceRepetitiveStrategy:
+        sample({
+          clock: trigger,
+          source: [$curTrigger, $source] as const,
+          filter: ([curTrigger]) => trigger === curTrigger,
+          fn: ([_, record]) => ({ record, trigger }),
+          target: replace,
+        });
+
+        sample({
+          clock: trigger,
+          source: [$curTrigger, $source] as const,
+          filter: ([curTrigger]) => trigger !== curTrigger,
+          fn: ([_, record]) => ({ record, trigger }),
+          target: push,
+        });
+        break;
+      default:
+        sample({
+          clock: trigger,
+          source: $source,
+          filter: $shouldPush,
+          fn: (record) => ({ record, trigger }),
+          target: push,
+        });
+    }
+  }
 
   $shouldPush.on(undo, () => false).on(redo, () => false);
 
@@ -106,6 +159,7 @@ export const createHistory = <T extends Record<string, Store<any>>>(params: {
   });
 
   for (const key in params.source) {
+    // @ts-expect-error
     params.source[key].on(syncTriggered, (_prev, obj) => obj[key]);
   }
 
@@ -117,14 +171,14 @@ export const createHistory = <T extends Record<string, Store<any>>>(params: {
     () => true
   );
 
-  // Initial clear
-  // Needed to push first state into history
-  clear();
-
   return {
     undo,
     redo,
-    push,
+    push: push.prepend((record: T) => ({ record, trigger: manualTrigger })),
+    replace: replace.prepend((record: T) => ({
+      record,
+      trigger: manualTrigger,
+    })),
     clear,
     $history,
     $curIndex,
@@ -132,5 +186,11 @@ export const createHistory = <T extends Record<string, Store<any>>>(params: {
     $length,
     $canUndo,
     $canRedo,
+    $actualState,
   };
 };
+
+/** Always pushes new records */
+export const pushStrategy: HistoryStrategy = {};
+/** Replace current record if it came from the same trigger. Push new otherwise */
+export const replaceRepetitiveStrategy: HistoryStrategy = {};
